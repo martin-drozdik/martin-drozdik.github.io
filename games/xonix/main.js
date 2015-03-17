7ed2eb854fccb842338a23e5d0bfa1f06b3a82c1
martin-drozdik <drozdik.svk@gmail.com>
2015-03-17 23:20:56 +0900

    various changes

--- games/xonix/main.js
+++ games/xonix/main.js
@@ -2,7 +2,7 @@
 
 ;(function()
 {
-    var gameState = {score: 0, lives: 3, level: 1, percentage: 0};
+    var gameState = {score: 0, lives: 3, level: 30, percentage: 0};
 
     window.onload = function()
     {
